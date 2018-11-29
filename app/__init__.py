958ffc7a85a33e517437f20a1bff71cb18605161
oliver <ozhang@ra.rockwell.com>
2018-11-29 09:10:55 +0800

    第二章：模板

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+#默认文件是ASCII格式，需要更改文件编码，操作是在文件首行加上
 from flask import Flask
 
 app = Flask(__name__)
+
+#将routes的导入放在底部可以避免由于这两个文件之间的相互引用而导致的错误
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+from flask import render_template
 from app import app
+
+#＠app.route行是装饰器
+#装饰器的常见模式是使用它们将函数注册为某些事件的回调函数
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    #return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    #render_template()函数调用Flask框架原生依赖的Jinja2模板引擎
+    #return render_template('index.html', title='Home',user=user)
+    #return render_template('index.html', user=user)
+    return render_template('index.html', title='Home',user=user, posts=posts)
