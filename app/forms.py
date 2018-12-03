90101416048685f80fe491c90569483930f7610c
oliver <ozhang@ra.rockwell.com>
2018-12-03 13:09:31 +0800

    第六章：个人主页和头像

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@
 # -*- coding:utf8 -*-
 from flask_wtf import FlaskForm
 #Flask-WTF插件本身不提供字段类型，因此我直接从WTForms包中导入了四个表示表单字段的类
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -28,3 +28,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
