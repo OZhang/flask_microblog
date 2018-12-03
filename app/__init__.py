b5975c09f8ad63b1553a7779fd40476d93d6573a
oliver <ozhang@ra.rockwell.com>
2018-12-03 09:42:50 +0800

    第五章：用户登录

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 #将routes的导入放在底部可以避免由于这两个文件之间的相互引用而导致的错误
 
 from app import routes, models
